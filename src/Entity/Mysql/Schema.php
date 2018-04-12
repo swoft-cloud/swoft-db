16dc0a9c5ffc9513e13489c35f3a5cd24ee2e51f
DeivaLiang <deiva@qq.com>
2018-04-12 15:49:27 +0800

    change decimal from NUMBER to FLOAT

--- src/Entity/Mysql/Schema.php
+++ src/Entity/Mysql/Schema.php
@@ -25,7 +25,7 @@ class Schema extends \Swoft\Db\Entity\Schema
         'datetime' => 'Types::DATETIME',
         'float'    => 'Types::FLOAT',
         'number'   => 'Types::NUMBER',
-        'decimal'  => 'Types::NUMBER',
+        'decimal'  => 'Types::FLOAT',
         'bool'     => 'Types::BOOLEAN',
         'tinyint'  => 'Types::INT',
     ];
@@ -41,7 +41,7 @@ class Schema extends \Swoft\Db\Entity\Schema
         'datetime' => self::TYPE_STRING,
         'float'    => self::TYPE_FLOAT,
         'number'   => self::TYPE_INT,
-        'decimal'  => self::TYPE_INT,
+        'decimal'  => self::TYPE_FLOAT,
         'bool'     => self::TYPE_BOOL,
         'tinyint'  => self::TYPE_INT
     ];
