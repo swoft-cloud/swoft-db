1ba3b6b537c2a7398a3e70a000bbbd03935d1c88
stelin <794774870@qq.com>
2019-04-12 21:49:34 +0800

    Merge commit '21fdf6df08655dc750a9e57ef50d67a90cf7e14c' as 'src/db'

--- src/http-message/test/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Composer autoload
-$autoloadFile = dirname(__DIR__) . '/vendor/autoload.php';
-if (file_exists($autoloadFile)) {
-    require_once $autoloadFile;
-}
--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,21 +1,11 @@
 <?php
-
-// vendor at component dir
-if (file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
-    require dirname(__DIR__) . '/vendor/autoload.php';
-    // application's vendor
-} elseif (file_exists(dirname(__DIR__, 3) . '/autoload.php')) {
-    /** @var \Composer\Autoload\ClassLoader $loader */
-    $loader = require dirname(__DIR__, 3) . '/autoload.php';
-
-    // need load test psr4 config map
-    $componentDir  = dirname(__DIR__);
-    $componentJson = $componentDir . '/composer.json';
-    $composerData  = json_decode(file_get_contents($componentJson), true);
-
-    foreach ($composerData['autoload-dev']['psr-4'] as $prefix => $dir) {
-        $loader->addPsr4($prefix, $componentDir . '/' . $dir);
-    }
-} else {
-    exit('Please run "composer install" to install the dependencies' . PHP_EOL);
+// Composer autoload
+$autoloadFile = dirname(__DIR__) . '/vendor/autoload.php';
+if (file_exists($autoloadFile)) {
+    require_once $autoloadFile;
 }
+
+\Swoole\Runtime::enableCoroutine();
+$application = new \Swoft\Test\TestApplication();
+$application->setBeanFile(__DIR__ . '/case/bean.php');
+$application->run();
